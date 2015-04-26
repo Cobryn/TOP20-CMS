1b49ba82290ac7f6382a73d03e1797921d089c2a
Virgil CAZAUBON <asso.oxweb@gmail.com>
2015-04-26 16:16:37 +0200

    Mise à jours 2 - Variable Database setting.php

--- include/setting.php
+++ include/setting.php
@@ -3,10 +3,16 @@
 // Merci de ne rien modifier de ce fichier.
 include('include/etat.php');
 include('include/info.php');
+include('include/database.php');
 if($etat == 1 ) {
 	$NomServeur = $server_name;
 	$IPServeur = $server_ip;
 	$PORTServeur = $server_port;
+	
+	$Hote = $database_host;
+	$Database = $database_database;
+	$User = $database_user;
+	$Password = $database_password; 
 } else {
 	header('location: install');
 }
